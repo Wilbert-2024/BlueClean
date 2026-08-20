6d9eda81ca36ae9b602762c497da1aa96be59c3f
ing.Martin <martin.gonzalezoro@gmail.com>
2026-08-20 11:21:07 -0600

    Se creo el archvio de Denucia_Modal

--- app/src/main/java/com/example/afinal/DB/modal/Denuncia_Modal.kt
+++ app/src/main/java/com/example/afinal/DB/modal/Denuncia_Modal.kt
@@ -1,4 +1,15 @@
 package com.example.afinal.DB.modal
 
+import com.google.firebase.Timestamp
+
 object Denuncia_Modal {
+    data class Datos(
+        val Barrio: String,
+        val Descripcion: String,
+        val Direccion: String,
+        val Estado : Boolean = false,
+        val Fecha_Hora: Timestamp = Timestamp.now(),
+        val Imagen: String,
+        val Tipo: String
+    )
 }
