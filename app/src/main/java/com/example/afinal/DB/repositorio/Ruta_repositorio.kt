42a5ade96270f7b166ab7d360e2da0c420d885ec
ing.Martin <martin.gonzalezoro@gmail.com>
2026-08-20 11:37:49 -0600

    obtener info y lista de rutas desde la coleccion Rutas

--- app/src/main/java/com/example/afinal/DB/repositorio/Ruta_repositorio.kt
+++ app/src/main/java/com/example/afinal/DB/repositorio/Ruta_repositorio.kt
@@ -1,4 +1,20 @@
 package com.example.afinal.DB.repositorio
 
+import com.example.afinal.DB.conexionBD.Conexion
+import com.example.afinal.DB.modal.Rutas_Modal
+
 object Ruta_repositorio {
+    private const val Coleccion = "Rutas"
+
+    fun obtener(onSuccess: (List<Rutas_Modal.Datos>) -> Unit) {
+        Conexion.db.collection(Coleccion).get()
+            .addOnSuccessListener { result ->
+                val lista = result.toObjects(Rutas_Modal.Datos::class.java)
+                onSuccess(lista)
+            }
+    }
+
+    fun agregar(ruta: Rutas_Modal.Datos) {
+        Conexion.db.collection(Coleccion).add(ruta)
+    }
 }
