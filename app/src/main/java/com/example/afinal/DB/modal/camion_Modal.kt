f68dad2a08343138e7360efd4c4fb37f3d026873
ing.Martin <martin.gonzalezoro@gmail.com>
2026-08-20 11:22:21 -0600

    Cracion de el archivo camion_Modal

--- app/src/main/java/com/example/afinal/DB/modal/camion_Modal.kt
+++ app/src/main/java/com/example/afinal/DB/modal/camion_Modal.kt
@@ -1,4 +1,21 @@
 package com.example.afinal.DB.modal
 
+import com.google.firebase.firestore.GeoPoint
+
 object camion_Modal {
+    data class DatosGenerales(
+        val Estado: Boolean = false,
+        val Placa: String,
+        val Ubicacion_actual: GeoPoint = GeoPoint(0.0, 0.0),
+        val ruta_id: String = ""
+    )
+
+    data class datosEscogidos(
+        val Placa: String="",
+        val ruta_id: String = ""
+    )
+
+
+
+
 }
