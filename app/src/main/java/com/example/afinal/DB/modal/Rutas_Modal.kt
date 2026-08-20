27e8655ed6ceb353b5d91680345154af64a936c5
ing.Martin <martin.gonzalezoro@gmail.com>
2026-08-20 11:20:06 -0600

    creacion del datos modal de Rutas

--- app/src/main/java/com/example/afinal/DB/modal/Rutas_Modal.kt
+++ app/src/main/java/com/example/afinal/DB/modal/Rutas_Modal.kt
@@ -1,4 +1,12 @@
 package com.example.afinal.DB.modal
 
 object Rutas_Modal {
+    data class Datos (
+        val Barrios: List<String> = emptyList(),
+        val Dias: List<String> = emptyList(),
+        val Estado: Boolean = false,
+        val Horario: Map<String, String> = emptyMap(),
+        val Nombre: String = "",
+        val Recorrido: Map<String, String> = emptyMap()
+    )
 }
