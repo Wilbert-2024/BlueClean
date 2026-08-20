e234cb36dd3d731837fc6f78f9d50cad44617794
ing.Martin <martin.gonzalezoro@gmail.com>
2026-08-20 11:24:46 -0600

    creacion de el archivo punto de referencia_modal

--- app/src/main/java/com/example/afinal/DB/modal/PuntoReferencia_Modal.kt
+++ app/src/main/java/com/example/afinal/DB/modal/PuntoReferencia_Modal.kt
@@ -1,4 +1,16 @@
 package com.example.afinal.DB.modal
 
+import com.google.firebase.firestore.GeoPoint
+
 object PuntoReferencia_Modal {
+
+    data class Lugar(
+        val Coordenadas: GeoPoint = GeoPoint(0.0, 0.0),
+        val Nombre: String = "",
+        val ruta_id: String = ""
+    )
+
+    data class Datos (
+        val Lugares: List<Lugar> = emptyList()
+    )
 }
