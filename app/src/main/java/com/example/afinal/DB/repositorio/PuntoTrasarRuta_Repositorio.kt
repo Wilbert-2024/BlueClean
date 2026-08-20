a6b53b9d7c335471ac57c197e2b69a3aa7d14039
ing.Martin <martin.gonzalezoro@gmail.com>
2026-08-20 11:37:41 -0600

    metodo para traer la lista de coordenadas geopoint de la coleccion Punto_trasar_ruta

--- app/src/main/java/com/example/afinal/DB/repositorio/PuntoTrasarRuta_Repositorio.kt
+++ app/src/main/java/com/example/afinal/DB/repositorio/PuntoTrasarRuta_Repositorio.kt
@@ -1,4 +1,39 @@
 package com.example.afinal.DB.repositorio
 
+import com.example.afinal.DB.conexionBD.Conexion
+import com.example.afinal.DB.modal.PuntoTrasarRuta_Modal
+import com.google.firebase.firestore.GeoPoint
+
 object PuntoTrasarRuta_Repositorio {
+    private const val Coleeccion = "Punto_trasar_ruta"
+
+
+    fun insertarPuntos(rutaId: String, Puntos: List<GeoPoint>, onSuppress: ()-> Unit, onError: ((Exception) -> Unit)? = null){
+        val datos = PuntoTrasarRuta_Modal.Datos(Puntos)
+
+        Conexion.db.collection(Coleeccion).document(rutaId).set(datos)
+            .addOnSuccessListener { onSuppress }
+            .addOnFailureListener { exception ->
+                onError?.invoke(exception)
+            }
+    }
+
+
+    fun obtenerCoordenadasPorRutas(rutaId: String, onSuccess: (List<GeoPoint>) -> Unit, onError: ((Exception)-> Unit)? = null){
+        Conexion.db.collection(Coleeccion).document(rutaId).get()
+            .addOnSuccessListener { doc ->
+                if (doc.exists()){
+                    val datos = doc.toObject(PuntoTrasarRuta_Modal.Datos::class.java)
+                    onSuccess(datos?.Coordenadas?: emptyList())
+
+                } else{ onSuccess(emptyList()) }
+            }
+            .addOnFailureListener { exception ->
+                onError?.invoke(exception)
+            }
+
+    }
+
+
+
 }
