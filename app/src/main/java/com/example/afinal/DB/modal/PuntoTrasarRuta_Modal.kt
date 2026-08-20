6d89db5ce284080d50ac967f215c98644679fa87
ing.Martin <martin.gonzalezoro@gmail.com>
2026-08-20 11:25:29 -0600

    Se creo el modal de los puntos que trasará las rutas en el mapa

--- app/src/main/java/com/example/afinal/DB/modal/PuntoTrasarRuta_Modal.kt
+++ app/src/main/java/com/example/afinal/DB/modal/PuntoTrasarRuta_Modal.kt
@@ -1,4 +1,9 @@
 package com.example.afinal.DB.modal
 
+import com.google.firebase.firestore.GeoPoint
+
 object PuntoTrasarRuta_Modal {
+    data class Datos (
+        val Coordenadas: List<GeoPoint> = emptyList()
+    )
 }
