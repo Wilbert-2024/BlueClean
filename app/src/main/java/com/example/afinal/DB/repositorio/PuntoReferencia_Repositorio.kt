514b6586ecd6b5eff648d317ad64ee660357ccab
ing.Martin <martin.gonzalezoro@gmail.com>
2026-08-20 11:37:31 -0600

    consultas para obtener nombres de lugares y coordenadas por barrio en Puntos_referencia

--- app/src/main/java/com/example/afinal/DB/repositorio/PuntoReferencia_Repositorio.kt
+++ app/src/main/java/com/example/afinal/DB/repositorio/PuntoReferencia_Repositorio.kt
@@ -1,4 +1,75 @@
 package com.example.afinal.DB.repositorio
 
+import com.example.afinal.DB.conexionBD.Conexion
+import com.example.afinal.DB.modal.PuntoReferencia_Modal
+
 object PuntoReferencia_Repositorio {
+
+    private val Coleccion = "Puntos_referencia"
+
+    fun agregar(barrio: String, datos: PuntoReferencia_Modal.Datos) {
+        Conexion.db.collection(Coleccion).document(barrio).set(datos)
+    }
+
+    fun obtenerTodo(onSuccess: (Map<String, List<PuntoReferencia_Modal.Lugar>>) -> Unit, onError: ((Exception) -> Unit)? = null){
+        Conexion.db.collection(Coleccion).get()
+            .addOnSuccessListener { resultadodetodo ->
+                val mapaBarrio = resultadodetodo.documents.associate { doc ->
+                    val datos = doc.toObject(PuntoReferencia_Modal.Datos::class.java)
+                    doc.id to (datos?.Lugares?: emptyList())
+                }
+                onSuccess(mapaBarrio)
+            }
+            .addOnFailureListener { exception ->
+                onError?.invoke(exception)
+            }
+    }
+
+
+    fun obtenerNombreDeZona(barrio: String, onSuccess: (List<String>) -> Unit){
+        Conexion.db.collection(Coleccion).document(barrio).get()
+            .addOnSuccessListener { doc ->
+                val datos = doc.toObject(PuntoReferencia_Modal.Datos::class.java)
+                val nombres = datos?.Lugares?.map { it.Nombre }?: emptyList()
+                onSuccess(nombres)
+            }
+            .addOnFailureListener { onSuccess(emptyList()) }
+    }
+
+
+    fun obtenerRutaPorBarrio(barrio: String, onSuccess: (List<String>) -> Unit, onError: ((Exception) -> Unit)? = null){
+        Conexion.db.collection(Coleccion).document(barrio).get()
+            .addOnSuccessListener { documento ->
+
+                if (documento.exists()){
+                    val datos = documento.toObject(PuntoReferencia_Modal.Datos::class.java)
+
+                    val rutaId = datos?.Lugares?.map {it.ruta_id}  ?.filter { it.isNotBlank() } ?.distinct()?: emptyList()
+                    onSuccess(rutaId)
+
+                } else {onSuccess(emptyList())}
+            }
+            .addOnFailureListener { exception ->
+                onError?.invoke(exception)
+            }
+    }
+
+    fun obtenerBarriosYcoordenadas(barrio: String, rutaId: String, onSuccess: (List<PuntoReferencia_Modal.Lugar>) -> Unit){
+        Conexion.db.collection(Coleccion).document(barrio).get()
+            .addOnSuccessListener { doc ->
+                val datos = doc.toObject(PuntoReferencia_Modal.Datos::class.java)
+                val lugaresCoordenadas = datos?.Lugares?.filter {
+                    it.ruta_id.equals(rutaId, ignoreCase = true)
+                } ?: emptyList()
+                onSuccess(lugaresCoordenadas)
+            }
+            .addOnFailureListener { onSuccess(emptyList()) }
+    }
+
+
+
+
+
+
+
 }
