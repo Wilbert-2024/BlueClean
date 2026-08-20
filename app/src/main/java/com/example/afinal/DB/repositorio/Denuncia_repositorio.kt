f182c9d2ed066ffea4af6ddd4a3025e20ad466ac
ing.Martin <martin.gonzalezoro@gmail.com>
2026-08-20 11:28:19 -0600

    se crea el archivo para hacr la consulta a la base de dato en la coleccion denuncia

--- app/src/main/java/com/example/afinal/DB/repositorio/Denuncia_repositorio.kt
+++ app/src/main/java/com/example/afinal/DB/repositorio/Denuncia_repositorio.kt
@@ -1,4 +1,13 @@
 package com.example.afinal.DB.repositorio
 
+import com.example.afinal.DB.conexionBD.Conexion
+import com.example.afinal.DB.modal.Denuncia_Modal
+
 object Denuncia_repositorio {
+    private const val Coleccion = "Denuncia"
+
+    fun insertar (denucia: Denuncia_Modal.Datos){
+        Conexion.db.collection(Coleccion).add(denucia)
+    }
+
 }
