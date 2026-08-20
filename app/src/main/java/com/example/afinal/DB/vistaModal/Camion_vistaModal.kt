3c912d60245249a83edb2c1857febb42e7d04fd3
ing.Martin <martin.gonzalezoro@gmail.com>
2026-08-20 11:38:18 -0600

    conectar camion_repositorio con la vista para mostrar los camiones

--- app/src/main/java/com/example/afinal/DB/vistaModal/Camion_vistaModal.kt
+++ app/src/main/java/com/example/afinal/DB/vistaModal/Camion_vistaModal.kt
@@ -4,14 +4,31 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import com.example.afinal.DB.repositorio.camion_repositprio
 import com.google.firebase.firestore.GeoPoint
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import com.example.afinal.DB.modal.camion_Modal
 
-class camion_visModal : ViewModel() {
+class Camion_vistaModal : ViewModel() {
     var ubicacion by mutableStateOf<GeoPoint?>(null)
         private set
+    var datosEscogidos by mutableStateOf<camion_Modal.datosEscogidos?>(null)
+        private set
+
 
     fun obtenerUbicacion(id: String) {
         camion_repositprio.obtenerUbicacion(id) { resultado ->
             ubicacion = resultado
         }
     }
+
+    fun obtenerDatosEscogidos(id: String) {
+        camion_repositprio.obtenerDatosEscogidos(id) { resultado ->
+            datosEscogidos = resultado
+        }
+    }
+
+
+
+
+
 }
