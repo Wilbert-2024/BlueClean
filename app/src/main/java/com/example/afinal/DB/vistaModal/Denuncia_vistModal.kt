2333f92f14c10c55951eacd3081e9b237886449d
ing.Martin <martin.gonzalezoro@gmail.com>
2026-08-23 22:58:43 -0600

    codgigo de vistaModalDenuncia

--- app/src/main/java/com/example/afinal/DB/vistaModal/Denuncia_vistModal.kt
+++ app/src/main/java/com/example/afinal/DB/vistaModal/Denuncia_vistModal.kt
@@ -3,25 +3,15 @@ package com.example.afinal.DB.vistaModal
 import androidx.lifecycle.ViewModel
 import com.example.afinal.DB.modal.Denuncia_Modal
 import com.example.afinal.DB.repositorio.Denuncia_repositorio
+import com.example.afinal.datos.Calendario.FechaHora
 
 class Denuncia_vistModal : ViewModel() {
 
-    fun insertarDenuncia(
-        barrio: String,
-        descripcion: String,
-        direccion: String,
-        imagen: String,
-        tipo: String,
+    fun insertarDenuncia(   barrio: String, descripcion: String,direccion: String, imagen: String,tipo: String,
         onSuccess: () -> Unit,
         onError: (Exception) -> Unit
     ) {
-        val nuevaDenuncia = Denuncia_Modal.Datos(
-            Barrio = barrio,
-            Descripcion = descripcion,
-            Direccion = direccion,
-            Imagen = imagen,
-            Tipo = tipo
-        )
+        val nuevaDenuncia = Denuncia_Modal.Datos(barrio,descripcion,direccion,Imagen = imagen, Tipo = tipo)
 
         Denuncia_repositorio.insertar(nuevaDenuncia, onSuccess, onError)
     }
