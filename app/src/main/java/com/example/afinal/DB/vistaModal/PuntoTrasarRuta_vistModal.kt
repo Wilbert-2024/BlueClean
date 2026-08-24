6066318dd241e5ace0dfdbc090b0794797600c41
ing.Martin <martin.gonzalezoro@gmail.com>
2026-08-23 23:02:01 -0600

    Agregar manejo de puntos de ruta en ViewModel

--- app/src/main/java/com/example/afinal/DB/vistaModal/PuntoTrasarRuta_vistModal.kt
+++ app/src/main/java/com/example/afinal/DB/vistaModal/PuntoTrasarRuta_vistModal.kt
@@ -1,4 +1,44 @@
 package com.example.afinal.DB.vistaModal
 
-class PuntoTrasarRuta_vistModal {
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import com.example.afinal.DB.repositorio.PuntoTrasarRuta_Repositorio
+import com.example.afinal.datosTemporal.TrasarLineas
+import com.google.firebase.firestore.GeoPoint
+
+class PuntoTrasarRuta_vistModal : ViewModel() {
+
+    var coordenadas by mutableStateOf<List<GeoPoint>>(emptyList())
+        private set
+
+
+    fun obtenerCoordenadasPorRuta(rutaId: String) {
+
+        PuntoTrasarRuta_Repositorio.obtenerCoordenadasPorRutas(
+            rutaId = rutaId,
+
+            onSuccess = { puntos ->
+                coordenadas = puntos
+            },
+
+            onError = { exception ->
+                exception.printStackTrace()
+            }
+        )
+    }
+
+
+    fun insertarPuntos( rutaId: String,  onSuccess: () -> Unit, onError: (Exception)-> Unit ) {
+
+        val puntos = TrasarLineas.coordenadas()
+        PuntoTrasarRuta_Repositorio.insertarPuntos(rutaId,puntos,
+            onSuppress = {  onSuccess()},
+
+            onError = { exception ->
+                onError(exception)
+            }
+        )
+    }
 }
