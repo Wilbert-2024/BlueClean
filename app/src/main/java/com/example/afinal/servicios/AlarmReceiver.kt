09e054f76218f61504a09522278705b885883934
ing.Martin <martin.gonzalezoro@gmail.com>
2026-08-25 20:42:23 -0600

    Configuracion de notificaion y sonido de la alarma

--- app/src/main/java/com/example/afinal/servicios/AlarmReceiver.kt
+++ app/src/main/java/com/example/afinal/servicios/AlarmReceiver.kt
@@ -4,11 +4,14 @@ import android.app.NotificationManager
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.media.RingtoneManager
+import android.util.Log
 import androidx.core.app.NotificationCompat
 import com.example.afinal.R
 
 class AlarmReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
+        Log.d("AlarmReceiver", "¡Alarma recibida! Procesando notificación...")
         val titulo = intent.getStringExtra("titulo") ?: "Recordatorio de Recolección"
         val mensaje = intent.getStringExtra("mensaje") ?: "¡El camión recolector está cerca! No olvides sacar tus bolsas."
         
@@ -17,12 +20,17 @@ class AlarmReceiver : BroadcastReceiver() {
 
     private fun mostrarNotificacion(context: Context, titulo: String, mensaje: String) {
         val notificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        val sonidoUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM)
         
-        val notification = NotificationCompat.Builder(context, "canal_alarmas")
-            .setSmallIcon(R.drawable.ic_launcher_foreground)
+        val notification = NotificationCompat.Builder(context, "canal_recoleccion_v3")
+            .setSmallIcon(android.R.drawable.ic_lock_idle_alarm)
             .setContentTitle(titulo)
             .setContentText(mensaje)
-            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setPriority(NotificationCompat.PRIORITY_MAX)
+            .setCategory(NotificationCompat.CATEGORY_ALARM)
+            .setSound(sonidoUri)
+            .setDefaults(NotificationCompat.DEFAULT_ALL)
+            .setVibrate(longArrayOf(0, 500, 200, 500))
             .setAutoCancel(true)
             .build()
 
