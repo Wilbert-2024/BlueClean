112d0c025f3e9aafa72398deaa2d2d5481f15141
ing.Martin <martin.gonzalezoro@gmail.com>
2026-08-31 00:21:02 -0600

    Se realizo a componer los nombre a como estan en la base de dato

--- app/src/main/java/com/example/afinal/DB/modal/PuntoTrasarRuta_Modal.kt
+++ app/src/main/java/com/example/afinal/DB/modal/PuntoTrasarRuta_Modal.kt
@@ -3,7 +3,7 @@ package com.example.afinal.DB.modal
 import com.google.firebase.firestore.GeoPoint
 
 object PuntoTrasarRuta_Modal {
-    data class Datos (
-        val Coordenadas: List<GeoPoint> = emptyList()
+    data class Datos(
+        val coordenadas: List<GeoPoint> = emptyList()
     )
 }
