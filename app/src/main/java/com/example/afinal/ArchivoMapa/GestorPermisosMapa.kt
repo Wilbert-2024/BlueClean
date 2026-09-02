968d9f48491a0bb58f91fe06ee0e6307068ad5d8
ing.Martin <martin.gonzalezoro@gmail.com>
2026-09-01 23:18:07 -0600

    Se anexó funciones para conprobar si hay internet como si esta activado la ubicacion o localizacion del telefono

--- app/src/main/java/com/example/afinal/ArchivoMapa/GestorPermisosMapa.kt
+++ app/src/main/java/com/example/afinal/ArchivoMapa/GestorPermisosMapa.kt
@@ -5,39 +5,39 @@ import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.location.LocationManager
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
 import android.provider.Settings
 import androidx.core.content.ContextCompat
 
-/**
- * Este objeto se encarga de gestionar los permisos de ubicación y el estado del GPS.
- * Permite que el usuario vea su posición real en el mapa de forma segura.
- */
 object GestorPermisosMapa {
 
-    /**
-     * Verifica si el usuario ya concedió el permiso de ubicación precisa.
-     */
+    /**pide permiso al usuario de acceder al la opcion del gps del telefono   */
     fun tienePermisoUbicacion(contexto: Context): Boolean {
-        return ContextCompat.checkSelfPermission(
-            contexto,
-            Manifest.permission.ACCESS_FINE_LOCATION
-        ) == PackageManager.PERMISSION_GRANTED
+        return ContextCompat.checkSelfPermission(  contexto,  Manifest.permission.ACCESS_FINE_LOCATION  ) == PackageManager.PERMISSION_GRANTED
     }
 
-    /**
-     * Verifica si el GPS (Ubicación) está encendido en los ajustes del teléfono.
-     */
+    /** Verifica si el GPS del telefono está encendido    */
     fun verificarGpsActivo(contexto: Context): Boolean {
         val administradorUbicacion = contexto.getSystemService(Context.LOCATION_SERVICE) as LocationManager
         return administradorUbicacion.isProviderEnabled(LocationManager.GPS_PROVIDER)
     }
 
-    /**
-     * Abre la pantalla de ajustes de ubicación del sistema Android.
-     */
+    /** Abre la pantalla de ajustes para activaer la ubicación del GPS en el teléfono. */
     fun abrirAjustesGps(contexto: Context) {
         val intent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
         contexto.startActivity(intent)
+    }
+
+
+    fun verificarConexionInternet(contexto: Context): Boolean {
+        val manejoConexion = contexto.getSystemService(Context.CONNECTIVITY_SERVICE) as? ConnectivityManager ?: return false
+        val redes = manejoConexion.activeNetwork ?: return false
+        val actNetwork = manejoConexion.getNetworkCapabilities(redes) ?: return false
+
+        return actNetwork.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) ||
+                actNetwork.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) ||
+                actNetwork.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET)
     }
 }
